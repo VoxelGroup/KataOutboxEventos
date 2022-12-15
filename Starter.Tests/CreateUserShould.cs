f14db28aadda114eeeb160ae766fe7ecfa10664e
Alex Pou <apou@voxelgroup.net>
2022-12-15 18:33:57 +0100

    refact: extract field

--- Starter.Tests/CreateUserShould.cs
+++ Starter.Tests/CreateUserShould.cs
@@ -12,36 +12,36 @@ namespace Starter.Tests
         private CreateUser _createUser;
         private IOutBoxRepository _outBoxRepository;
         private IDbConnection _idbConnection;
+        private IDbTransaction _dbTransaction;
 
         public CreateUserShould()
         {
             _userRepository = Substitute.For<IUserRepository>();
             _outBoxRepository = Substitute.For<IOutBoxRepository>();
             _idbConnection = Substitute.For<IDbConnection>();
+            _dbTransaction = Substitute.For<IDbTransaction>();
             _createUser = new CreateUser(_userRepository, _outBoxRepository, _idbConnection);
         }
         
         [Fact]
         public void CreateUser()
         {
-            var dbTransaction = Substitute.For<IDbTransaction>();
-            _idbConnection.BeginTransaction().Returns(dbTransaction);
+            _idbConnection.BeginTransaction().Returns(_dbTransaction);
             
             _createUser.Execute();
             
-            _userRepository.Received(1).SaveUser(Arg.Any<User>(), dbTransaction);
+            _userRepository.Received(1).SaveUser(Arg.Any<User>(), _dbTransaction);
         }
 
         [Fact]
         public void Save()
         {
-            var dbTransaction = Substitute.For<IDbTransaction>();
-            _idbConnection.BeginTransaction().Returns(dbTransaction);
+            _idbConnection.BeginTransaction().Returns(_dbTransaction);
             
             _createUser.Execute();
             
-            _outBoxRepository.Received(1).SaveEvent(Arg.Any<OutboxRecord>(), dbTransaction);
-            dbTransaction.Received(1).Commit();
+            _outBoxRepository.Received(1).SaveEvent(Arg.Any<OutboxRecord>(), _dbTransaction);
+            _dbTransaction.Received(1).Commit();
         }
         
     }
